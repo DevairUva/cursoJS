efb01a04bb8dca0c361f329e9326888655ec629a
Devair Martins <devair.martins@gmail.com>
2022-11-14 16:00:11 -0300

    estrangeiro

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let nome = prompt ('nome do turista');
 let opcao = prompt('Já visitou alguma cidade?');
-let cidades = prompt('escreva o nome da cidade');
+let cont = 0;
 
-while(opcao !== 'não'){
-    opcao = prompt('Já visitou alguma outra cidade?');
-    if(opcao === "sim"){
-        cidades += ', '+prompt('escreva o nome da cidade');
-    } else {
-        alert('ok')
+if(opcao === "sim"){
+    cont++;
+    let cidades = prompt('escreva o nome da cidade');
+    while(opcao !== 'não'){
+        opcao = prompt('Já visitou alguma outra cidade?');
+        if(opcao === "sim"){
+            cidades += '\n'+prompt('escreva o nome da cidade');
+            cont++;
+        } else {
+            alert('ok')
+        }
     }
+    alert(nome+' visitou um total de '+cont+' cidade(s).\nSão elas: : \n'+cidades);
+} else {
+    alert('Poxa, '+nome+'. Você precisa curtir mais a vida!')
 }
-
-alert(nome+' já visitou as seguintes cidades: \n'+cidades);
