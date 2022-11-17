784c2e29a05edfd607dcadf536ed9bca8ec207ff
Devair Martins <devair.martins@gmail.com>
2022-11-17 11:04:17 -0300

    tabuada e palíndromo

--- script.js
+++ script.js
@@ -20,27 +20,58 @@
 // }
 
 
-let inicial = parseInt(prompt('digite a quantidade inicial'));
-let atual = parseInt(inicial);
-let add = parseInt(0);
-let sub = parseInt(0);
-let opcao = 0;
+// let inicial = parseInt(prompt('digite a quantidade inicial'));
+// let atual = parseInt(inicial);
+// let add = parseInt(0);
+// let sub = parseInt(0);
+// let opcao = 0;
 
-do{
-    opcao = parseInt(prompt('Você tem R$'+atual+'. O que deseja fazer?\n1)Aumentar\n2)Diminuir\n3)Sair'));
-    switch(opcao){
-        case 1:
-            add = parseInt(prompt('Aumentar quanto?'));
-            atual = parseInt(atual+add);
-            break;
-        case 2:
-            sub = prompt('Diminuir quanto?');
-            atual = atual-sub;
-            break;
-        case 3:
-            alert("Saindo do menu.")
-            break
-        default:
-            alert('Opção inválida.')
-    }
-}while(opcao != 3);
+// do{
+//     opcao = parseInt(prompt('Você tem R$'+atual+'. O que deseja fazer?\n1)Aumentar\n2)Diminuir\n3)Sair'));
+//     switch(opcao){
+//         case 1:
+//             add = parseInt(prompt('Aumentar quanto?'));
+//             atual = parseInt(atual+add);
+//             break;
+//         case 2:
+//             sub = prompt('Diminuir quanto?');
+//             atual = atual-sub;
+//             break;
+//         case 3:
+//             alert("Saindo do menu.")
+//             break
+//         default:
+//             alert('Opção inválida.')
+//     }
+// }while(opcao != 3);
+
+//TABOADA
+// let numero = prompt('Deseja a taboada de qual número?');
+// let resposta = ''
+
+// for(i=1; i<21; i++){
+//     let resultado = numero*i;
+//     resposta += numero+" x "+i+" = "+resultado+"\n";
+// }
+
+// alert(resposta);
+
+//Palíndromo
+// let nome = prompt('Digite uma palavra para descobrir se a mesma é um palíndromo:');
+// let contrario = '' ;
+// let j = nome.length-1;
+
+// for(i=0; i<nome.length; i++){
+//     contrario += nome[j];
+//     j--;
+// }
+
+// if(nome == contrario){
+//     alert(nome+' é um palíndromo.');
+// } else {
+//     alert(
+//         'O nome = '+nome+
+//         '\nO nome ao contrário = '+contrario
+//     )
+// }
+
